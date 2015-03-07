00391e278db6de5973febcba46c491610cb15af2
Alexmackzie <alexmackzie@hotmail.com>
2015-03-07 19:17:44 +0100

    the paddle no longer jitters when it hits the end of the map.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,24 +3,20 @@ using System.Collections;
 
 public class PlayerController : MonoBehaviour {
 
-	public float speed;
+	public float speed = 1f;
+
+	private Vector3 playerPos = new Vector3 (0, 0, 0);
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start () {		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-	}
+		float xPos = transform.position.x + (Input.GetAxis("Horizontal") * speed);
+		playerPos = new Vector3 (Mathf.Clamp (xPos, -26f, 26f), 0, 0);
+		transform.position = playerPos;
 
-	void FixedUpdate() {
-		float moveHorizontal = Input.GetAxis ("Horizontal");
+}
 
-		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, 0.0f);
-		rigidbody.velocity = movement * speed;
-
-
-	}
 }
