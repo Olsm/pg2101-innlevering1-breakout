bf8f9408f114d3ff3d0d64b9b393a721b65253b9
Alexmackzie <alexmackzie@hotmail.com>
2015-03-08 20:24:50 +0100

    no message

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,11 +2,11 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-
+	
 	public float speed = 1f;
-
+	
 	private Vector3 playerPos = new Vector3 (0, 0, 0);
-
+	
 	// Use this for initialization
 	void Start () {		
 	}
@@ -16,7 +16,7 @@ public class PlayerController : MonoBehaviour {
 		float xPos = transform.position.x + (Input.GetAxis("Horizontal") * speed);
 		playerPos = new Vector3 (Mathf.Clamp (xPos, -26f, 26f), 0, 0);
 		transform.position = playerPos;
-
-}
-
+		
+	}
+	
 }
